5d05c8978637ee2a6c6e05af2f3eb8b0274729ff
ENZO <enzo.zyh@gmail.com>
2016-03-31 22:39:56 +0200

    ...

--- app/src/main/java/com/example/enzo/practicedemos/Activities/Main2Activity.java
+++ app/src/main/java/com/example/enzo/practicedemos/Activities/Main2Activity.java
@@ -3,6 +3,8 @@ package com.example.enzo.practicedemos.Activities;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.example.enzo.practicedemos.R;
+
 public class Main2Activity extends AppCompatActivity {
 
     @Override
